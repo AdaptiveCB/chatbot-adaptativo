849aa3b2f2c38da0037033477da55f0466a063c0
Your Name <you@example.com>
2019-06-24 19:35:38 -0500

    qaa

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 CORS(app)
 
 app.config['MONGO_DBNAME'] = 'chatbot'
-# app.config['MONGO_URI'] = 'mongodb+srv://chatbot:adaptive@cluster0-k4fnb.mongodb.net/chatbot?retryWrites=true&w=majority'
-app.config['MONGO_URI'] = 'mongodb://localhost/chatbot'
+app.config['MONGO_URI'] = 'mongodb+srv://chatbot:adaptive@cluster0-k4fnb.mongodb.net/chatbot?retryWrites=true&w=majority'
+# app.config['MONGO_URI'] = 'mongodb://localhost/chatbot'
 
 mongo = PyMongo(app)
 
