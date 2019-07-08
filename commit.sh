fdf3157ed293ef4b559ab006ef268d524969a182
unknown <mitsuoysharag@gmail.com>
2019-07-08 00:08:52 -0500

    make it better

--- commit.sh
+++
@@ -1,6 +0,0 @@
-# heroku login
-# heroku git:clone -a adaptive-chatbot
-
-git add .
-git commit -am "make it better"
-git push heroku master
---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# heroku login
+# heroku git:clone -a adaptive-chatbot
+
+git add .
+git commit -m "make it better"
+
+# git remote rm origin
+# git remote add origin https://github.com/AdaptiveCB/chatbot-adaptativo.git
+
+git push origin master
