daee6808dbc17d23432adb016797bef0fd0ccaab
luis <luiscarlos_0831@hotmail.com>
2019-10-27 10:00:52 -0500

    entidad

--- app.py
+++ app.py
@@ -587,6 +587,85 @@ def eliminarCurso():
 
   return jsonify(objetoResultado)
 
+# CRUD ENTIDAD
+
+@app.route('/obtenerEntidadPorTema', methods=['GET','POST'])
+def obtenerEntidadPorTema():
+  data = request.get_json()
+
+  tema_id = data['tema_id']
+
+  coleccionEntidad = mongo.db.entidad
+  
+  entidades = coleccionEntidad.find({'tema_id':ObjectId(tema_id)})
+
+  entidades = dumps(entidades)
+
+  return jsonify(entidades)
+
+@app.route('/ingresarEntidad',methods=['POST'])
+def ingresarEntidad():
+  data = request.get_json()
+
+  nombre = data['nombre']
+  tema_id = data['tema_id']
+
+  coleccionEntidad = mongo.db.entidad
+
+  entidadIngresada = coleccionEntidad.insert_one({
+    "nombre":nombre,
+    "tema_id":ObjectId(tema_id)
+  }).inserted_id
+
+  entidad = {
+    'entidad_id' : str(entidadIngresada)
+  }
+
+  return jsonify(entidad)
+
+@app.route('/actualizarEntidad',methods=['POST'])
+def actualizarEntidad():
+  data = request.get_json()
+
+  entidad_id = data['entidad_id']
+  nombre = data['nombre']
+  tema_id = data['tema_id']
+  
+  coleccionEntidad = mongo.db.entidad
+
+  resultado = coleccionEntidad.update_one(
+    {'_id': ObjectId(entidad_id)},
+    {'$set':  
+              { 
+                'nombre': nombre,
+                'tema_id': ObjectId(tema_id)
+              }
+    }
+  )
+
+  objetoResultado = {
+    'encontrado': resultado.matched_count,
+    'modificado': resultado.modified_count
+  }
+
+  return jsonify(objetoResultado)
+
+@app.route('/eliminarEntidad',methods=['POST'])
+def eliminarEntidad():
+  data = request.get_json()
+
+  entidad_id = data['entidad_id']
+
+  coleccionEntidad = mongo.db.entidad
+
+  resultado = coleccionEntidad.delete_one({'_id': ObjectId(entidad_id)})
+
+  objetoResultado = {
+    'eliminado': resultado.deleted_count
+  }
+
+  return jsonify(objetoResultado)
+
 # CRUD ALUMNO
 
 @app.route('/obtenerAlumnosPorCurso', methods=['POST'])
@@ -679,6 +758,7 @@ def obtenerAlumnoPorNombre():
     })
     objetoAlumno = {
       'alumno_id': str(alumno['_id']),
+      'nombre': alumno['nombre'],
       'apellido_paterno': alumno['apellido_paterno'],
       'apellido_materno': alumno['apellido_materno'],
       'codigo': alumno['codigo']
